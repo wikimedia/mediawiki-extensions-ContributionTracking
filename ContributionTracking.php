deffe409af4e536cea07d9185d204f02e43a24a5
Tyler Romeo <tylerromeo@gmail.com>
2015-05-24 17:51:42 +0200

    Updated extension registration to new system
    Added extension.json file and added shim to main entry point.
    
    Bug: T87903
    Change-Id: Ib2f1c5f93b243ec79e25b580b3ba933a22d796ea

--- ContributionTracking.php
+++ ContributionTracking.php
@@ -1,13 +1,19 @@
 <?php
-
-# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo <<<EOT
-To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/ContributionTracking/ContributionTracking.php" );
-EOT;
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'ContributionTracking' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['ContributionTracking'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['ContributionTracking'] = __DIR__ . '/ContributionTracking.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for ContributionTracking extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
 }
+
+/**
+ * Setup for pre-1.25 wikis. Make sure this is kept in sync with extension.json
+ */
 
 $wgExtensionCredits['specialpage'][] = array(
 	'path'           => __FILE__,
@@ -21,6 +27,7 @@ $dir = __DIR__ . '/';
 
 $wgMessagesDirs['ContributionTracking'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['ContributionTrackingAlias'] = $dir . 'ContributionTracking.alias.php';
+$wgAutoloadClasses['ContributionTrackingHooks'] = $dir . 'ContributionTracking.hooks.php';
 $wgAutoloadClasses['ContributionTracking'] = $dir . 'ContributionTracking_body.php';
 $wgSpecialPages['ContributionTracking'] = 'ContributionTracking';
 
@@ -38,7 +45,7 @@ $wgAutoloadClasses['ApiContributionTracking'] = $dir . 'ApiContributionTracking.
 $wgAutoloadClasses['ContributionTrackingProcessor'] = $dir . 'ContributionTracking.processor.php';
 
 //this only works if contribution tracking is inside a mediawiki DB, which typically it isn't.
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'efContributionTrackingLoadUpdates';
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'ContributionTrackingHooks::onLoadExtensionSchemaUpdates';
 
 // Resource modules
 $ctResourceTemplate = array(
@@ -100,64 +107,3 @@ $wgContributionTrackingFundraiserMaintenanceUnsched = false;
 
 // api modules
 $wgAPIModules['contributiontracking'] = 'ApiContributionTracking';
-
-/**
- * @param $updater DatabaseUpdater
- * @return bool
- */
-function efContributionTrackingLoadUpdates( $updater = null ){
-
-	$dir = dirname( __FILE__ ) . '/';
-	if ( $updater === null ) {
-		global $wgExtNewTables, $wgExtNewFields;
-
-		$wgExtNewTables[] = array( 'contribution_tracking', $dir . 'ContributionTracking.sql' );
-		$wgExtNewTables[] = array( 'contribution_tracking_owa_ref', $dir . 'ContributionTracking_OWA_ref.sql' );
-
-		$wgExtNewFields[] = array(
-			'contribution_tracking',
-			'owa_session',
-			$dir . 'patch-owa.sql',
-		);
-	} else {
-		global $wgContributionTrackingDBname;
-
-		if( $updater->getDB()->getDBname() === $wgContributionTrackingDBname ) {
-			$updater->addExtensionTable( 'contribution_tracking', $dir . 'ContributionTracking.sql' );
-			$updater->addExtensionTable( 'contribution_tracking_owa_ref', $dir . 'ContributionTracking_OWA_ref.sql' );
-			$updater->addExtensionUpdate( array( 'addField', 'contribution_tracking', 'owa_session',
-				$dir . 'patches/patch-owa.sql', true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'contribution_tracking', 'utm_key',
-				$dir . 'patches/patch-owa.sql', true ) );
-
-			if ( $updater->getDB()->getType() !== 'sqlite' ) {
-				// Not sure how to do this in the other configurations, but I guess
-				// "manually" might be the answer.
-				$updater->addExtensionUpdate( array( 'modifyField', 'contribution_tracking', 'anonymous',
-					$dir . 'patches/make-null.patch.sql', true ) );
-			}
-		} else { //We are configured not to use the main mediawiki db.
-			//Unless the updater is modified not to run
-			//'LoadExtensionSchemaUpdates' hooks in its constructor (or do so
-			//conditionally), we're going to have to do these manually.
-			$ctDB = ContributionTrackingProcessor::contributionTrackingConnection();
-			if ( !$ctDB->tableExists( 'contribution_tracking' ) ){
-				$ctDB->sourceFile( $dir . 'ContributionTracking.sql' );
-			}
-			if ( !$ctDB->tableExists( 'contribution_tracking_owa_ref' ) ){
-				$ctDB->sourceFile( $dir . 'ContributionTracking_OWA_ref.sql' );
-			}
-			if ( !$ctDB->fieldExists( 'contribution_tracking', 'owa_session' ) ){
-				$ctDB->sourceFile( $dir . 'patches/patch-owa.sql' );
-			}
-			if ( !$ctDB->fieldExists( 'contribution_tracking', 'utm_key' ) ){
-				$ctDB->sourceFile( $dir . 'patches/patch-utm_key.sql' );
-			}
-			if ( !$ctDB->fieldExists( 'contribution_tracking', 'country' ) ){
-				$ctDB->sourceFile( $dir . 'patches/20120924.new_columns.sql' );
-			}
-		}
-	}
- 	return true;
-
-}
