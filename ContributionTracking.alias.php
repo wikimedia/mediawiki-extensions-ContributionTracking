10fc16589cab64b2efc9c8203411f4bfb9451d38
Elliott Eggleston <ejegg@ejegg.com>
2018-07-17 16:15:15 -0500

    Delete obsolete parts of this extension.
    This extension is now only used for the FundraiserMaintenance page
    and to manage a database connection.
    
    Change-Id: I4e7e0bfde9882b054cadc544852339f8b2fe02a6

--- ContributionTracking.alias.php
+++ ContributionTracking.alias.php
@@ -10,218 +10,84 @@ $specialPageAliases = [];
 
 /** English (English) */
 $specialPageAliases['en'] = [
-	'ContributionTracking' => [ 'ContributionTracking' ],
 	'FundraiserMaintenance' => [ 'FundraiserMaintenance' ],
-	'ContributionTrackingTester' => [ 'ContributionTrackingTester' ],
 ];
 
 /** Arabic (العربية) */
 $specialPageAliases['ar'] = [
-	'ContributionTracking' => [ 'تتبع_المساهمة' ],
 	'FundraiserMaintenance' => [ 'صيانة_التبرع' ],
 ];
 
 /** Egyptian Arabic (مصرى) */
 $specialPageAliases['arz'] = [
-	'ContributionTracking' => [ 'متابعة_المساهمه' ],
 	'FundraiserMaintenance' => [ 'صيانه_التبرع' ],
 ];
 
 /** Western Balochi (بلوچی رخشانی) */
 $specialPageAliases['bgn'] = [
-	'ContributionTracking' => [ 'پدگیری_شه_شراکتان' ],
 	'FundraiserMaintenance' => [ 'بُنیادین_ساتیتین' ],
-];
-
-/** Bosnian (bosanski) */
-$specialPageAliases['bs'] = [
-	'ContributionTracking' => [ 'DoprinosiPracenje' ],
 ];
 
 /** German (Deutsch) */
 $specialPageAliases['de'] = [
-	'ContributionTracking' => [ 'Beiträge_verfolgen', 'Beitragstracking', 'Beitragsverfolgung' ],
 	'FundraiserMaintenance' => [ 'Kampagnenverwaltung' ],
 ];
 
 /** Zazaki (Zazaki) */
 $specialPageAliases['diq'] = [
-	'ContributionTracking' => [ 'Teqibêİştırakan' ],
 	'FundraiserMaintenance' => [ 'WeynayışaBécan' ],
-];
-
-/** Lower Sorbian (dolnoserbski) */
-$specialPageAliases['dsb'] = [
-	'ContributionTracking' => [ 'Slědowanje_pśinoskow' ],
-];
-
-/** Esperanto (Esperanto) */
-$specialPageAliases['eo'] = [
-	'ContributionTracking' => [ 'Spurado_de_kontribuoj', 'Kontribukontado' ],
 ];
 
 /** Persian (فارسی) */
 $specialPageAliases['fa'] = [
-	'ContributionTracking' => [ 'ردگیری_مشارکت' ],
 	'FundraiserMaintenance' => [ 'نگهداری_پایه‌ای' ],
-];
-
-/** Finnish (suomi) */
-$specialPageAliases['fi'] = [
-	'ContributionTracking' => [ 'Muokkausten_seuranta' ],
-];
-
-/** Galician (galego) */
-$specialPageAliases['gl'] = [
-	'ContributionTracking' => [ 'Seguimento_das_contribucións' ],
 ];
 
 /** Hebrew (עברית) */
 $specialPageAliases['he'] = [
-	'ContributionTracking' => [ 'מעקב_תרומות' ],
 	'FundraiserMaintenance' => [ 'תחזוקת_התרמה' ],
-];
-
-/** Haitian (Kreyòl ayisyen) */
-$specialPageAliases['ht'] = [
-	'ContributionTracking' => [ 'SwiviKontribisyon' ],
-];
-
-/** Hungarian (magyar) */
-$specialPageAliases['hu'] = [
-	'ContributionTracking' => [ 'Adományok_követése' ],
-];
-
-/** Interlingua (interlingua) */
-$specialPageAliases['ia'] = [
-	'ContributionTracking' => [ 'Traciamento_de_contributiones' ],
-];
-
-/** Indonesian (Bahasa Indonesia) */
-$specialPageAliases['id'] = [
-	'ContributionTracking' => [ 'Pelacakan_kontribusi', 'PelacakanKontribusi' ],
-];
-
-/** Italian (italiano) */
-$specialPageAliases['it'] = [
-	'ContributionTracking' => [ 'TracciaEdit' ],
 ];
 
 /** Japanese (日本語) */
 $specialPageAliases['ja'] = [
-	'ContributionTracking' => [ '寄付追跡' ],
 	'FundraiserMaintenance' => [ '資金調達保守' ],
 ];
 
 /** Korean (한국어) */
 $specialPageAliases['ko'] = [
-	'ContributionTracking' => [ '기여추적' ],
 	'FundraiserMaintenance' => [ '기금마련유지보수' ],
-];
-
-/** Colognian (Ripoarisch) */
-$specialPageAliases['ksh'] = [
-	'ContributionTracking' => [ 'MetmaacherVerfoljung' ],
-];
-
-/** Ladino (Ladino) */
-$specialPageAliases['lad'] = [
-	'ContributionTracking' => [ 'PerseguirAjustamientos' ],
 ];
 
 /** Northern Luri (لۊری شومالی) */
 $specialPageAliases['lrc'] = [
-	'ContributionTracking' => [ 'دئماگئری_ھومیاری' ],
 	'FundraiserMaintenance' => [ 'ڤاداشت_پاکون_دیارکون' ],
-];
-
-/** Minangkabau (Baso Minangkabau) */
-$specialPageAliases['min'] = [
-	'ContributionTracking' => [ 'LacakJariah' ],
 ];
 
 /** Macedonian (македонски) */
 $specialPageAliases['mk'] = [
-	'ContributionTracking' => [ 'СледењеНаПридонесите' ],
 	'FundraiserMaintenance' => [ 'ОдржувањеПрибирањеСредства' ],
-];
-
-/** Malayalam (മലയാളം) */
-$specialPageAliases['ml'] = [
-	'ContributionTracking' => [ 'സേവനംശ്രദ്ധിക്കൽ' ],
-];
-
-/** Marathi (मराठी) */
-$specialPageAliases['mr'] = [
-	'ContributionTracking' => [ 'योगदानमागमूस' ],
-];
-
-/** Norwegian Bokmål (norsk bokmål) */
-$specialPageAliases['nb'] = [
-	'ContributionTracking' => [ 'Bidragssporing' ],
 ];
 
 /** Low Saxon (Netherlands) (Nedersaksies) */
 $specialPageAliases['nds-nl'] = [
-	'ContributionTracking' => [ 'Donasies_volgen' ],
 	'FundraiserMaintenance' => [ 'Fondsenwarvingsbeheer' ],
 ];
 
 /** Dutch (Nederlands) */
 $specialPageAliases['nl'] = [
-	'ContributionTracking' => [ 'DonatiesVolgen' ],
 	'FundraiserMaintenance' => [ 'Fondsenwervingsbeheer' ],
-];
-
-/** Polish (polski) */
-$specialPageAliases['pl'] = [
-	'ContributionTracking' => [ 'Przekierowanie_do_systemu_płatnościowego' ],
-];
-
-/** Sanskrit (संस्कृतम्) */
-$specialPageAliases['sa'] = [
-	'ContributionTracking' => [ 'योगदानउन्नयति' ],
-];
-
-/** Sicilian (sicilianu) */
-$specialPageAliases['scn'] = [
-	'ContributionTracking' => [ 'TracciaEdit' ],
-];
-
-/** Slovak (slovenčina) */
-$specialPageAliases['sk'] = [
-	'ContributionTracking' => [ 'SledovaniePríspevkov' ],
 ];
 
 /** Swedish (svenska) */
 $specialPageAliases['sv'] = [
-	'ContributionTracking' => [ 'Bidragsspårning' ],
 	'FundraiserMaintenance' => [ 'Insamlingshantering' ],
 ];
-
-/** Tagalog (Tagalog) */
-$specialPageAliases['tl'] = [
-	'ContributionTracking' => [ 'Pagbakas_ng_ambag' ],
-];
-
-/** Turkish (Türkçe) */
-$specialPageAliases['tr'] = [
-	'ContributionTracking' => [ 'KatkıTakibi' ],
-];
-
-/** Venetian (vèneto) */
-$specialPageAliases['vec'] = [
-	'ContributionTracking' => [ 'TraciamentoContributi' ],
-];
-
 /** Simplified Chinese (中文（简体）‎) */
 $specialPageAliases['zh-hans'] = [
-	'ContributionTracking' => [ '资助追踪', '贡献追踪' ],
 	'FundraiserMaintenance' => [ '募捐工具维护' ],
 ];
 
 /** Traditional Chinese (中文（繁體）‎) */
 $specialPageAliases['zh-hant'] = [
-	'ContributionTracking' => [ '追蹤貢獻' ],
 	'FundraiserMaintenance' => [ '募捐維護' ],
 ];
