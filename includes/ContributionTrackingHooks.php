c58459b26528624f914dc1b88a42d3f100fe2c6a
Umherirrender <umherirrender_de.wp@web.de>
2019-11-03 12:34:29 +0100

    Remove use of $wgExtNewTables from updater code
    Due to type hint for $updater, that variable is never null
    
    Change-Id: I7eb42ca7e827b6e1cc3e0b6d7007e22198b81d2e

--- includes/ContributionTrackingHooks.php
+++ includes/ContributionTrackingHooks.php
@@ -3,67 +3,54 @@
 class ContributionTrackingHooks {
 
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
+		global $wgContributionTrackingDBname;
+
 		$dir = dirname( __DIR__ ) . '/';
-		if ( $updater === null ) {
-			global $wgExtNewTables, $wgExtNewFields;
 
-			$wgExtNewTables[] = [ 'contribution_tracking', $dir . 'ContributionTracking.sql' ];
-			$wgExtNewTables[] = [ 'contribution_tracking_owa_ref',
-				$dir . 'ContributionTracking_OWA_ref.sql' ];
+		if (
+			$updater->getDB()->getDBname() === $wgContributionTrackingDBname ||
+			$wgContributionTrackingDBname === null
+		) {
+			$updater->addExtensionTable( 'contribution_tracking',
+				$dir . 'ContributionTracking.sql' );
+			$updater->addExtensionTable( 'contribution_tracking_owa_ref',
+				$dir . 'ContributionTracking_OWA_ref.sql' );
+			$updater->addExtensionUpdate( [ 'addField', 'contribution_tracking',
+				'owa_session', $dir . 'patches/patch-owa.sql', true ] );
+			$updater->addExtensionUpdate( [ 'addField', 'contribution_tracking', 'utm_key',
+				$dir . 'patches/patch-owa.sql', true ] );
 
-			$wgExtNewFields[] = [
-				'contribution_tracking',
-				'owa_session',
-				$dir . 'patch-owa.sql',
-			];
+			if ( $updater->getDB()->getType() !== 'sqlite' ) {
+				// Not sure how to do this in the other configurations, but I guess
+				// "manually" might be the answer.
+				$updater->addExtensionUpdate( [ 'modifyField', 'contribution_tracking',
+					'anonymous', $dir . 'patches/make-null.patch.sql', true ] );
+			}
+			if ( $updater->getDB()->getType() !== 'postgres' ) {
+				$updater->addExtensionUpdate( [ 'addTable', 'contribution_tracking',
+					$dir . 'ContributionTracking.pg.sql', true ] );
+			}
 		} else {
-			global $wgContributionTrackingDBname;
-
-			if (
-				$updater->getDB()->getDBname() === $wgContributionTrackingDBname ||
-				$wgContributionTrackingDBname === null
-			) {
-				$updater->addExtensionTable( 'contribution_tracking',
-					$dir . 'ContributionTracking.sql' );
-				$updater->addExtensionTable( 'contribution_tracking_owa_ref',
-					$dir . 'ContributionTracking_OWA_ref.sql' );
-				$updater->addExtensionUpdate( [ 'addField', 'contribution_tracking',
-					'owa_session', $dir . 'patches/patch-owa.sql', true ] );
-				$updater->addExtensionUpdate( [ 'addField', 'contribution_tracking', 'utm_key',
-					$dir . 'patches/patch-owa.sql', true ] );
-
-				if ( $updater->getDB()->getType() !== 'sqlite' ) {
-					// Not sure how to do this in the other configurations, but I guess
-					// "manually" might be the answer.
-					$updater->addExtensionUpdate( [ 'modifyField', 'contribution_tracking',
-						'anonymous', $dir . 'patches/make-null.patch.sql', true ] );
-				}
-				if ( $updater->getDB()->getType() !== 'postgres' ) {
-					$updater->addExtensionUpdate( [ 'addTable', 'contribution_tracking',
-						$dir . 'ContributionTracking.pg.sql', true ] );
-				}
-			} else {
-				// We are configured not to use the main mediawiki db.
-				// Unless the updater is modified not to run
-				// 'LoadExtensionSchemaUpdates' hooks in its constructor (or do so
-				// conditionally), we're going to have to do these manually.
-				$ctDB = ContributionTrackingProcessor::contributionTrackingConnection();
-				'@phan-var \Wikimedia\Rdbms\IMaintainableDatabase $ctDB';
-				if ( !$ctDB->tableExists( 'contribution_tracking' ) ) {
-					$ctDB->sourceFile( $dir . 'ContributionTracking.sql' );
-				}
-				if ( !$ctDB->tableExists( 'contribution_tracking_owa_ref' ) ) {
-					$ctDB->sourceFile( $dir . 'ContributionTracking_OWA_ref.sql' );
-				}
-				if ( !$ctDB->fieldExists( 'contribution_tracking', 'owa_session' ) ) {
-					$ctDB->sourceFile( $dir . 'patches/patch-owa.sql' );
-				}
-				if ( !$ctDB->fieldExists( 'contribution_tracking', 'utm_key' ) ) {
-					$ctDB->sourceFile( $dir . 'patches/patch-utm_key.sql' );
-				}
-				if ( !$ctDB->fieldExists( 'contribution_tracking', 'country' ) ) {
-					$ctDB->sourceFile( $dir . 'patches/20120924.new_columns.sql' );
-				}
+			// We are configured not to use the main mediawiki db.
+			// Unless the updater is modified not to run
+			// 'LoadExtensionSchemaUpdates' hooks in its constructor (or do so
+			// conditionally), we're going to have to do these manually.
+			$ctDB = ContributionTrackingProcessor::contributionTrackingConnection();
+			'@phan-var \Wikimedia\Rdbms\IMaintainableDatabase $ctDB';
+			if ( !$ctDB->tableExists( 'contribution_tracking' ) ) {
+				$ctDB->sourceFile( $dir . 'ContributionTracking.sql' );
+			}
+			if ( !$ctDB->tableExists( 'contribution_tracking_owa_ref' ) ) {
+				$ctDB->sourceFile( $dir . 'ContributionTracking_OWA_ref.sql' );
+			}
+			if ( !$ctDB->fieldExists( 'contribution_tracking', 'owa_session' ) ) {
+				$ctDB->sourceFile( $dir . 'patches/patch-owa.sql' );
+			}
+			if ( !$ctDB->fieldExists( 'contribution_tracking', 'utm_key' ) ) {
+				$ctDB->sourceFile( $dir . 'patches/patch-utm_key.sql' );
+			}
+			if ( !$ctDB->fieldExists( 'contribution_tracking', 'country' ) ) {
+				$ctDB->sourceFile( $dir . 'patches/20120924.new_columns.sql' );
 			}
 		}
 		return true;
